d50dd5ea3fafc62d67cb532822cce2666f12603e
VidhurKariminlla <117694881+VidhurKariminlla@users.noreply.github.com>
2023-08-22 12:08:36 -0500

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,9 +1,9 @@
 function addUser()
 {
-    user_name = document.getElementById("user_name").ariaValueMax;
+    user_name = document.getElementById("user_name").value;
 
     localStorage.setItem("user_name" , user_name)
 
-    window_location = "kwitter_room.html"
+    window.location = "kwitter_room.html"
 }
 
